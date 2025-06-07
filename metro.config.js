67e6b3b774757c34196905848bbd4820e099ff9c
sabri <sabrina.oliveira.clemente@gmail.com>
2025-06-06 22:46:38 -0300

    chore: setup project lib

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,16 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {withNativeWind} = require('nativewind/metro');
+const {getDefaultConfig,mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const config = mergeConfig(getDefaultConfig(__dirname),{
+	resolver: {
+		extraNodeModules: {
+			'react-dom': path.resolve(__dirname,'shim','react-dom.js'),
+		},
+	},
+	watchFolders: [path.resolve(__dirname,'shim')],
+});
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = withNativeWind(config,{
+	input: './global.css',
+});
